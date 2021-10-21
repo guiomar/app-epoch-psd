298665e2adaeaad63e971180bd0dcf7e5ad792f1
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-10-21 13:57:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,24 +30,12 @@ with open(__location__+'/config.json') as config_json:
 fdir = config['output']
 fname = os.listdir(fdir)[0]
 # Rename files so is MNE compliant
-epoch = mne.read_epochs(os.path.join(fdir,fname))
+epochs = mne.read_epochs(os.path.join(fdir,fname))
 
 
 # == GET CONFIG VALUES ==
-
 fmin = config['fmin']
 fmax = config['fmax']
-
-
-if config['picks']:
-    #If its a list starting with square braket, convert to list of strings
-    if config['picks'].find("[") == 0:
-        picks = config['picks'].replace('[','').replace(']','').split(", ")
-    else:
-        picks = config['picks']   
-else: 
-    picks=None
-
 # Advanced parameters
 tmin=config['tmin'] if config['tmin'] else None
 tmax=config['tmax'] if config['tmax'] else None
@@ -58,111 +46,119 @@ low_bias=True,
 normalization='length'
 
 proj = config['proj']
-#n_jobs = config['n_jobs']
-#verbose = config['verbose']
+n_jobs=8
 
-print(tmin)
-print(picks)
+# Picks
+if config['picks']:
+    #If its a list starting with square braket, convert to list of strings
+    if config['picks'].find("[") == 0:
+        picks = config['picks'].replace('[','').replace(']','').split(", ")
+    else:
+        picks = config['picks']   
+else: 
+    picks=None
 
 
-# == GET SELECTED CHANNELS ==
-
-# Find selected channels indexes
-info=epoch.info
-# If picks is left to by default (GUIO) -- USAR PICKS CASO GENERICO!!
-if picks==None:
-    ichan = mne.pick_types(info, meg=True, eeg=True, ref_meg=False, exclude=info['bads'])
-    # Get channel names
-    canales = np.take(epoch.ch_names,ichan)
-else:
-    canales=picks
+# == COMPUTE PSD ==
 
 # psd_welch.shape = Nepochs x Nchannels x Nfreqs
 
-# == COMPUTE PSD ==
+ch_types=np.unique(epochs.get_channel_types())
+
+
+
 if picks==None:
 
-    picks_grad='grad'
-    psd_welch_grad, freqs_grad = mne.time_frequency.psd_multitaper(epoch, fmin=fmin, fmax=fmax, tmin=tmin, tmax=tmax, 
-                            bandwidth=bandwidth, adaptive=adaptive, low_bias=low_bias, normalization=normalization, 
-                            picks=picks_grad, proj=proj, n_jobs=1, verbose=None)
-    # Convert power to dB scale.
-    psd_welch_grad = 10*(np.log10(psd_welch_grad.mean(axis=0)*1e13**2)) ## (T/m)^2/hz -> (fT/cm)^2/Hz
-
-    picks_mag='mag'
-    psd_welch_mag, freqs_mag = mne.time_frequency.psd_multitaper(epoch, fmin=fmin, fmax=fmax, tmin=tmin, tmax=tmax, 
-                            bandwidth=bandwidth, adaptive=adaptive, low_bias=low_bias, normalization=normalization, 
-                            picks=picks_mag, proj=proj, n_jobs=1, verbose=None)
-    # Convert power to dB scale.
-    psd_welch_mag = 10*(np.log10(psd_welch_mag.mean(axis=0)*1e15**2)) # T^2/hz -> fT^2/Hz
-
-
-    psd_welch = np.concatenate((psd_welch_grad,psd_welch_mag), axis=0)
-    if freqs_grad.all()==freqs_mag.all(): 
-        freqs=freqs_grad
-    cangrad = fnmatch.filter(canales, '*[23]')
-    canmag =  fnmatch.filter(canales, '*1')
-    canales = cangrad + canmag
-
     # FIGURE 1
-    # Plot computed Welch PSD
+    # Number of subplots
+    bb=0
+    for i in ['grad','mag','eeg']: 
+        if i in ch_types: bb=bb+1
     plt.figure(1)
-    fig, axs = plt.subplots(2)
+    fig, axs = plt.subplots(bb)
+    aa=0
 
-    axs[0].plot(freqs_grad, psd_welch_grad.mean(axis=0).transpose(), zorder=1) 
-    axs[0].set_xlim(xmin=0, xmax=max(freqs_grad))
-    axs[0].set_xlabel('Frequency (Hz)')
-    axs[0].set_ylabel('PSD - grad')
+    if 'grad' in ch_types:
+        epochs_grad = epochs.copy().pick('grad')
+        ch_grad = epochs_grad.ch_names
+        psd_welch_grad, freqs_grad = mne.time_frequency.psd_multitaper(epochs_grad, 
+                            fmin=fmin, fmax=fmax, tmin=tmin, tmax=tmax, 
+                            bandwidth=bandwidth, adaptive=adaptive, low_bias=low_bias, normalization=normalization, 
+                            picks='grad', proj=proj, n_jobs=n_jobs, verbose=None)
+        # Convert power to dB scale: (T/m)^2/hz -> (fT/cm)^2/Hz
+        psd_welch_grad = 10*(np.log10(psd_welch_grad.mean(axis=0)*1e13**2))
 
-    axs[1].plot(freqs_mag, psd_welch_mag.mean(axis=0).transpose(), zorder=1) 
-    axs[1].set_xlim(xmin=0, xmax=max(freqs_mag))
-    axs[1].set_xlabel('Frequency (Hz)')
-    axs[1].set_ylabel('PSD - mag')
+        # Save to CSV file (could be also TSV)
+        df_psd = pd.DataFrame(psd_welch_grad, index=ch_grad, columns=freqs_grad)
+        df_psd.index.name='channels'
+        df_psd.to_csv(os.path.join('out_dir','psd_grad.csv')) #, sep = '\t', index=False)
+
+        # Figure
+        axs[aa].plot(freqs_grad, psd_welch_grad.mean(axis=0).transpose(), zorder=1) 
+        axs[aa].set_xlim(xmin=0, xmax=max(freqs_grad))
+        axs[aa].set_xlabel('Frequency (Hz)')
+        axs[aa].set_ylabel('PSD - grad: (fT/cm)^2/Hz [dB]')
+        aa=aa+1
+
+    if 'mag' in ch_types:
+        epochs_mag = epochs.copy().pick('mag')
+        ch_mag = epochs_mag.ch_names
+        psd_welch_mag, freqs_mag = mne.time_frequency.psd_multitaper(epochs_mag, 
+                            fmin=fmin, fmax=fmax, tmin=tmin, tmax=tmax, 
+                            bandwidth=bandwidth, adaptive=adaptive, low_bias=low_bias, normalization=normalization, 
+                            picks='mag', proj=proj, n_jobs=n_jobs, verbose=None)
+        # Convert power to dB scale: T^2/hz -> fT^2/Hz
+        psd_welch_mag = 10*(np.log10(psd_welch_mag.mean(axis=0)*1e15**2))
+
+        # Save to CSV file (could be also TSV)
+        df_psd = pd.DataFrame(psd_welch_mag, index=ch_mag, columns=freqs_mag)
+        df_psd.index.name='channels'
+        df_psd.to_csv(os.path.join('out_dir','psd_mag.csv')) #, sep = '\t', index=False)
+
+        # Figure
+        axs[aa].plot(freqs_mag, psd_welch_mag.mean(axis=0).transpose(), zorder=1) 
+        axs[aa].set_xlim(xmin=0, xmax=max(freqs_mag))
+        axs[aa].set_xlabel('Frequency (Hz)')
+        axs[aa].set_ylabel('PSD - mag: fT^2/Hz [dB]')
+        aa=aa+1
+
+
+    if 'eeg' in ch_types:
+        epochs_eeg = epochs.copy().pick('eeg')
+        ch_eeg = epochs_eeg.ch_names
+        psd_welch_eeg, freqs_eeg = mne.time_frequency.psd_multitaper(epochs_eeg, 
+                            fmin=fmin, fmax=fmax, tmin=tmin, tmax=tmax, 
+                            bandwidth=bandwidth, adaptive=adaptive, low_bias=low_bias, normalization=normalization, 
+                            picks='eeg', proj=proj, n_jobs=n_jobs, verbose=None)
+        # Convert power to dB scale: XXXXX
+        psd_welch_eeg = 10*(np.log10(psd_welch_eeg.mean(axis=0)*1e15**2))
+
+        # Save to CSV file (could be also TSV)
+        df_psd = pd.DataFrame(psd_welch_eeg, index=ch_eeg, columns=freqs_eeg)
+        df_psd.index.name='channels'
+        df_psd.to_csv(os.path.join('out_dir','psd_eeg.csv')) #, sep = '\t', index=False)
+
+        # Figure
+        axs[aa].plot(freqs_eeg, psd_welch_eeg.mean(axis=0).transpose(), zorder=1) 
+        axs[aa].set_xlim(xmin=0, xmax=max(freqs_eeg))
+        axs[aa].set_xlabel('Frequency (Hz)')
+        axs[aa].set_ylabel('PSD - eeg: xxxx [dB]')
+        aa=aa+1
 
     # Save fig
     plt.savefig(os.path.join('out_figs','psd_computed.png'))
-
-else:
-    #SPECIFIC CHANNELS
-    psd_welch, freqs = mne.time_frequency.psd_multitaper(epoch, fmin=fmin, fmax=fmax, tmin=tmin, tmax=tmax, 
-                            bandwidth=bandwidth, adaptive=adaptive, low_bias=low_bias, normalization=normalization, 
-                            picks=picks, proj=proj, n_jobs=1, verbose=None)
-
-    # Convert power to dB scale.
-    psd_welch = 10*(np.log10(psd_welch.mean(axis=0)) + (2*15)) #psd_welch*(10**(2*15)) // psd_welch*1e30  # T**2/hz -> fT**2/Hz
-
-    # FIGURE 1
-    # Plot computed Welch PSD
-    plt.figure(1)
-    plt.plot(freqs, psd_welch.transpose(), zorder=1) 
-    plt.xlim(xmin=0, xmax=max(freqs))
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Power Spectral Density')
-    plt.title('Computed PSD')
-    # Save fig
-    plt.savefig(os.path.join('out_figs','psd_computed.png'))
-
-
-
-# == SAVE FILE ==
-# Save to CSV file (could be also TSV)
-df_psd = pd.DataFrame(psd_welch, index=canales, columns=freqs)
-df_psd.index.name='channels'
-df_psd.to_csv(os.path.join('out_dir','psd.csv')) #, sep = '\t', index=False)
-
-# Read CSV file
-#df = pd.read_csv("df_psd.csv")
-#print(df)
 
 
 # ==== PLOT FIGURES ====
 # FIGURE 2
 # Plot MNE PSD
 plt.figure(2)
-epoch.plot_psd(tmin=tmin, tmax=tmax, fmin=fmin, fmax=fmax, proj=proj, n_fft=n_fft, n_overlap=n_overlap, window=window, 
+figs_mne = epochs.plot_psd(tmin=tmin, tmax=tmax, fmin=fmin, fmax=fmax, 
+            proj=proj, n_fft=n_fft, n_overlap=n_overlap, window=window, 
             ax=None, color='black', xscale='linear', area_mode='std', area_alpha=0.33, 
-            dB=True, estimate='auto', show=True, n_jobs=1, average=False, 
+            dB=True, estimate='auto', show=True, n_jobs=n_jobs, average=False, 
             line_alpha=None, spatial_colors=True, sphere=None, verbose=None)
+
 # Save fig
 plt.savefig(os.path.join('out_figs','psd_mne.png'))
 
